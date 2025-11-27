733a752dace5735f29a3818d0512fabdc8c488cc
Kanami Anderson <kanamianderson@gmail.com>
2025-11-27 12:45:59 -0600

    Starting the tree sum challenge now. I tried it out using an iterative solution using both the stack and the queue and was able to figure it out. Next I will learn how do to this recursively.

--- vanilla-js-practice.js
+++ vanilla-js-practice.js
@@ -6,44 +6,40 @@ class Node {
     }
 }
 
-const breadthFirstValues = (root) => {
+const treeSum = (root) => {
     if (root === null) return [];
-
-    const result = []
-    const queue = [ root ]
-    while (queue.length > 0) {
-        const current = queue.shift();
-        result.push(current.val);
-        if (current.left !== null) queue.push(current.left);
-        if (current.right !== null) queue.push(current.right);    
+    let result = 0
+    const stack = [ root ]
+    while (stack.length > 0) {
+        const current = stack.pop();
+        result += current.val;
+        if (current.right !== null) stack.push(current.right);    
+        if (current.left !== null) stack.push(current.left);
+        
     }
     return result;
 }
 
 
 
-const a = new Node('a');
-const b = new Node('b');
-const c = new Node('c');
-const d = new Node('d');
-const e = new Node('e');
-const f = new Node('f');
-const g = new Node('g');
+const a = new Node(3);
+const b = new Node(11);
+const c = new Node(4);
+const d = new Node(4);
+const e = new Node(-2);
+const f = new Node(1);
 
 a.left = b;
 a.right = c;
 b.left = d;
 b.right = e;
 c.right = f;
-e.left = g;
 
-        //     a
-        //    / \
-        //   b   c
-        //  / \   \
-        // d   e   f
-        //    /    
-        //   g
+//       3
+//    /    \
+//   11     4
+//  / \      \
+// 4   -2     1
 
-console.log(breadthFirstValues(a));
+console.log(treeSum(a)); // -> 21
 
