00c7e4f5b95b740075028199489e77daa28c19db
Kanami Anderson <kanamianderson@gmail.com>
2025-11-27 12:03:00 -0600

    Solved the challenge recursively.

--- vanilla-js-practice.js
+++ vanilla-js-practice.js
@@ -6,42 +6,36 @@ class Node {
     }
 }
 
-
-
 const depthFirstValues = (root) => {
     if (root === null) return [];
-    const result = [];
-    const stack = [ root ];
-    while (stack.length > 0 ) {
-       const current = stack.pop();
-       result.push(current.val);
-      
-       if (current.right) stack.push(current.right);
-       if (current.left) stack.push(current.left);
-       
-    };
-    return result;
+    const leftValues = depthFirstValues(root.left);
+    const rightValues = depthFirstValues(root.right);
+    return [ root.val, ...leftValues, ...rightValues];
 }
 
-const a = new Node('a');
-// const b = new Node('b');
-// const c = new Node('c');
-// const d = new Node('d');
-// const e = new Node('e');
-// const f = new Node('f');
-// const g = new Node('g');
 
-// a.left = b;
-// a.right = c;
-// b.left = d;
-// b.right = e;
-// c.right = f;
-// e.left = g;
+
+const a = new Node('a');
+const b = new Node('b');
+const c = new Node('c');
+const d = new Node('d');
+const e = new Node('e');
+const f = new Node('f');
+const g = new Node('g');
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+e.left = g;
 
         //     a
         //    / \
         //   b   c
         //  / \   \
         // d   e   f
+        //    /    
+        //   g
 
-console.log(depthFirstValues(null))
+console.log(depthFirstValues(a))
