4bd54d4d2f72e46e68dbaf3239c18c3b63b31db4
Kanami Anderson <kanamianderson@gmail.com>
2025-11-28 15:28:35 -0600

    Starting a new TypeScript course with Mosh, and am trying to follow along with VS Code. It's hard and I am struggling with the configurations.

---
+++ hello-world/src/index.ts
@@ -0,0 +1,13 @@
+let sales = 123_456_789;
+let course = "TypeScript";
+let is_published = true;
+
+function render(document: any) {
+    console.log(document);
+
+    let numbers: number[] = []
+    numbers.forEach(n => n.toString)
+
+    let user: [number, string] = [1, 'Mosh'];
+    user[0].toExponential
+    user[1].indexOf
