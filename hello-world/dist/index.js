858920a69bb25565cff5285106eb9e061b811671
Kanami Anderson <kanamianderson@gmail.com>
2025-11-30 17:14:34 -0600

    Just ended this course with Optional Property Access Operator.

--- hello-world/dist/index.js
+++ hello-world/dist/index.js
@@ -1,10 +1,9 @@
 "use strict";
+var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
-function greet(name) {
-    if (name)
-        console.log(name.toLocaleUpperCase);
-    else
-        console.log('Hola!');
+function getCustomer(id) {
+    return id === 0 ? null : { birthday: new Date() };
 }
-greet(null);
+let customer = getCustomer(1);
+console.log((_a = customer === null || customer === void 0 ? void 0 : customer.birthday) === null || _a === void 0 ? void 0 : _a.getFullYear());
 //# sourceMappingURL=index.js.map
